07e8855d9b785312f19920ddee1dc7fa168a2fc3
james <james@example.com>
2019-03-05 17:58:34 +0800

    dfa

--- m/wjaction/default/Index.class.php
+++ m/wjaction/default/Index.class.php
@@ -4,5 +4,15 @@ class Index extends WebBase{
 	public final function main(){		
 		$this->display('index.php');
 	}
-	
+    public final function create_time(){
+	    echo time();
+        $type = 50;
+        $datas=$this->getRows("select * from {$this->prename}data_time where type={$type} order by actionNo asc");
+        echo '[';
+        foreach ($datas as $data) {
+            echo '"'.$data['actionTime'].'"'.',';
+        }
+        echo ']';
+        //$this->getRow();
+    }
 }
